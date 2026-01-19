92dcb71d6f1913bec1ddf23ae7fc38fd215e3714
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2026-01-19 23:34:54 +0530

    addint images and files to the repo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
+<<<<<<< HEAD
 document.querySelectorAll("button").forEach(btn => {
   btn.addEventListener("click", () => {
     console.log("Button clicked!");
   });
 });
+=======
+>>>>>>> 385dacd (adding new images and modifying files in repo)
